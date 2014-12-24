1ea24307e5b333cef4f881e3c519e1de13428cfc
ErwanDavid <erwan.dd@gmail.com>
2014-12-24 08:11:22 +0100

    Create launcher.pl

---
+++ launcher.pl
@@ -0,0 +1,21 @@
+my @port_list = ('2221', '2222', '2223', '2280', '28080', '22139', '22445');
+
+print "Lanceur !\n";
+
+foreach (@port_list)
+{
+        my $cmd = 'perl ./run_honey.pl '.$_.' localhost hihoney  Coll_1209 &';
+        print "\t - $cmd\n";
+        system ($cmd) ;
+
+}
+
+my $joined = join(' or port ', @port_list);
+
+my $cmd_tcp = "tcpdump -qn 'tcp and port " . $joined . "' | perl ./parser3.pl  &";                                                                                                                                               
+#tcpdump -qn 'tcp and port not 22 and port not 445 and port not 27017' | perl ./parser3.pl &                                                                                                                                     
+print "- $cmd_tcp\n";                                                                                                                                                                                                            
+                                                                                                                                                                                                                                 
+system ($cmd_tcp) ;                                                                                                                                                                                                              
+                                                                                                                                                                                                                                 
+print "Done!\n";               
