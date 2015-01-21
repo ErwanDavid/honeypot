4692d90adfa92e8f97df34dc2685e02b277d6aa5
ErwanDavid <erwan.dd@gmail.com>
2015-01-21 21:53:51 +0100

    Update launcher.pl

--- launcher.pl
+++ launcher.pl
@@ -1,21 +1,19 @@
 my @port_list = ('2221', '2222', '2223', '2280', '28080', '22139', '22445');
 
-print "Lanceur !\n";
+print "Go !\n";
 
 foreach (@port_list)
 {
-        my $cmd = 'perl ./run_honeypot.pl '.$_.' localhost hihoney  Coll_1209 &';
+        my $cmd = 'perl ./run_honeypot.pl '.$_.' localhost mongodatabase  mongocollection &';
         print "\t - $cmd\n";
         system ($cmd) ;
 
 }
 
 my $joined = join(' or port ', @port_list);
-
 my $cmd_tcp = "tcpdump -qn 'tcp and port " . $joined . "' | perl ./parser_tcp.pl  &";                                                                                                                                               
 #tcpdump -qn 'tcp and port not 22 and port not 445 and port not 27017' | perl ./parser3.pl &                                                                                                                                     
-print "- $cmd_tcp\n";                                                                                                                                                                                                            
-                                                                                                                                                                                                                                 
+print "- $cmd_tcp\n";
 system ($cmd_tcp) ;                                                                                                                                                                                                              
                                                                                                                                                                                                                                  
 print "Done!\n";               
